0ed3bacd178a0b718ab6ec3e84f795e928190123
Faith6888 <faharuna003@st.ug.edu.gh>
2026-08-02 00:09:00 +0000

    assignment_03_array_statistics.cpp

--- assignment_03_array_statistics.cpp
+++ assignment_03_array_statistics.cpp
@@ -42,3 +42,78 @@
 #include <iostream>
 using namespace std;
 
+int computeSum(int arr[], int n)
+{
+    int sum = 0;
+    for (int i = 0; i < n; i++)
+    {
+        sum += arr[i];
+    }
+    return sum;
+}
+
+double computeAverage(int sum, int n)
+{
+    return (double)sum / n;
+}
+
+int computeMax(int arr[], int n)
+{
+    int max = arr[0];
+    for (int i = 1; i < n; i++)
+    {
+        if (arr[i] > max)
+        {
+            max = arr[i];
+        }
+    }
+    return max;
+}
+
+int computeMin(int arr[], int n)
+{
+    int min = arr[0];
+    for (int i = 1; i < n; i++)
+    {
+        if (arr[i] < min )
+        {
+            min = arr[i];
+        }
+    }
+    return min;
+}
+
+int main()
+{
+    int n; 
+    cout << " How many numbers do you want to enter? ";
+    cin >> n;
+
+    if (n <= 0)
+    {
+        cout << "Error: N must be a positie number (a number greater than 0)" <<endl;
+        return 0;
+    }
+
+    int numbers [n];
+    for (int i = 0; i < n; i++)
+    {
+        cout << " Please enter a number " << ( i + 1 ) << ": ";
+        cin >> numbers[i];
+    }
+
+    int sum = computeSum(numbers, n);
+    double average = computeAverage(sum, n);
+    int maximum = computeMax(numbers, n);
+    int minimum = computeMin(numbers, n);
+    
+    cout << "\nResults: " <<endl;
+    cout << "Sum: " << sum << endl;
+    cout << "Average: " << average << endl;
+    cout << "Maximum: " << maximum << endl;
+    cout << "Minimum: " << minimum << endl;
+
+    return 0;
+
+
+}
